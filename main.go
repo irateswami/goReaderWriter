2bd7be85e7a36d87909597296134884b03e19811
Bryan English <bryanenglish@protonmail.com>
2020-05-28 16:28:35 -0500

    added some functions

--- main.go
+++ main.go
@@ -1,6 +1,20 @@
 package main
 
 import "fmt"
+import "io"
+
+type thingy struct {
+	data1 int
+	data2 string
+}
+
+func (t *thingy) modify(in io.Reader) error {
+
+	var err error
+	t.data1 = int(in)
+	return err
+
+}
 
 func main() {
 	fmt.Println("vim-go")
